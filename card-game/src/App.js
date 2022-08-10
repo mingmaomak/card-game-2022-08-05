afdfdd09f1f3512c1882888583a6e1c3e7e46d03
Jo <mingmaomak@gmail.com>
2022-08-11 02:03:19 +0700

    จะสังเกตุได้ว่า มีการ import คอมโพเนนต์ CharacterCard มาจากอีกไฟล์ CharacterCard.js เพื่อน าคอมโพเนนต์มาใช้ใน คอมโพเนนต์ App และมีการส่งผ่าน props ชื่อ value จากคอมโพเนนต์ App ไปยังคอมโพเนนต์ CharacterCard ในกรณีที่เราต้องการวนซ ้าเพื่อสร้างคอมโพเนนต์หลายๆ คอมโพเนนต์พร้อมกัน ก็สามารถท าได้โดยการเขียนโค้ด JavaScript ลงไปใน JSX ได้ โดยแก้ไขไฟล์App.js

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
  return (
  <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
  </div>
  );
 }
+//* การใส่ Props ชื่อว่า Key จะช่วยในกระบวนการอ้างอิงของ React ในกรณีวนซ ้าเพื่อสร้างคอมโพเนนต
 export default App;
