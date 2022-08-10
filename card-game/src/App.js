152210458f3a956cb45d7273b389d1788e06edbb
Jo <mingmaomak@gmail.com>
2022-08-11 02:19:31 +0700

    ซ่อม App.js
    OwO

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -5,7 +5,7 @@ import CharacterCard from './CharacterCard';
 const word = "Hello";
 function App() {
  return (
- <div className = {`card`}>
+ <div>
  {
  Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
  }
